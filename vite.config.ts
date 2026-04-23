9f6e7a0665813d2d57224038417163fc84cca91f
Emmanuel <emmanuelsol9302004@gmail.com>
2026-04-23 22:12:48 +0800

    asdas

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Daily-Progress-Report/',
+  base: './',
   plugins: [
     react(),
   ],
